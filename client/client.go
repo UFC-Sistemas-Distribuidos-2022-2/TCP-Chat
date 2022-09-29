4236696c6f57533bcab270c46c1735246f39e287
unknown <victorehrich@gmail.com>
2022-09-29 18:39:57 -0300

    C:/Program Files/Git/help works, /sair works, /usuario works

--- client/client.go
+++ client/client.go
@@ -14,31 +14,42 @@ const prefix = "/"
 
 func main() {
 
-	var comand string = ""
+	//var comand string = ""
 	var IPPort string = ""
-	var msg string = ""
+	var try = false
 	fmt.Println("Bem vindo ao chat TCP")
-	fmt.Println("Deseja entrar no chat? Digite /ENTRAR para começar")
-	for comand != "/ENTRAR" {
-		fmt.Scan(&comand)
-	}
+	// fmt.Println("Deseja entrar no chat? Digite /ENTRAR para começar")
+	// for comand != "/ENTRAR" {
+	// 	fmt.Scan(&comand)
+	// }
 
-	fmt.Println("Informe o ip e a porta que deseja tentar se conectar no formato IP:PORTA")
-	fmt.Scan(&IPPort)
+	// fmt.Println("Informe o ip e a porta que deseja tentar se conectar no formato IP:PORTA")
+	// fmt.Scan(&IPPort)
+	fmt.Println("Me informa também seu nome de usuário:")
+	var username string
+	fmt.Scanln(&username)
+	IPPort = "127.0.0.1:1234"
 	p := make([]byte, 2048)
 	conn, err := net.Dial("tcp", IPPort)
 	if err != nil {
-		fmt.Printf("Some error %v", err)
-		return
+		try = true
+		for try {
+			fmt.Printf("Some error %v\n", err)
+			fmt.Println("Informe o ip e a porta que deseja tentar se conectar no formato IP:PORTA")
+			fmt.Scan(&IPPort)
+			fmt.Println("Me informa também seu nome de usuário")
+			fmt.Scan(&username)
+			conn, err = net.Dial("tcp", IPPort)
+			if err == nil {
+				try = false
+			}
+		}
 	}
-
-	conn.Read((p))
-	fmt.Printf("%s", p)
-	var username string
-	fmt.Scan(&username)
+	for username == "" {
+		fmt.Scanln(&username)
+	}
+	conn.Write([]byte(username))
 	var address []string = strings.Split(conn.LocalAddr().String(), ":")
-	p, err = jsonBuilder(username, msg, address[0], address[1])
-	conn.Write([]byte(p))
 	wg.Add(1)
 	go Read(conn, p)
 	go Write(conn, username, address[0], address[1])
@@ -63,15 +74,18 @@ func Write(conn net.Conn, username string, ip string, port string) {
 	for {
 		//var msg string
 		//fmt.Scan(&msg)
-		in := bufio.NewReader(os.Stdin)
+		var in *bufio.Reader
+		in = bufio.NewReader(os.Stdin)
 
-		line, err := in.ReadString('\n')
-		p, err := jsonBuilder(username, line, ip, port)
-		if err != nil {
-			fmt.Printf("Some error %v", err)
-			return
+		line, _ := in.ReadString('\n')
+		if line != "\n" {
+			p, err := jsonBuilder(username, line, ip, port)
+			if err != nil {
+				fmt.Printf("Some error %v", err)
+				return
+			}
+			conn.Write(p)
 		}
-		conn.Write(p)
 	}
 
 }
--- server/server.go
+++ server/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"strings"
 
 	"bufio"
 	"fmt"
@@ -10,13 +11,12 @@ import (
 
 const welcomeMsg = "Olá, por favor, me informa seu nome de usuário:"
 
-var comands [3]string = [3]string{"/ENTRAR", "/SAIR", "/USUARIOS"}
+var comands [3]string = [3]string{"/HELP", "/SAIR", "/USUARIOS"}
 var conns []*net.TCPConn
 
 func main() {
 	fmt.Println("server test")
 
-	p := make([]byte, 2048)
 	addr := net.TCPAddr{
 		Port: 1234,
 		IP:   net.ParseIP("127.0.0.1"),
@@ -27,49 +27,94 @@ func main() {
 		return
 	}
 	defer ser.Close()
-
+	var users []Users
+	var user Users
 	for {
+		p := make([]byte, 2048)
 		conn, err := ser.AcceptTCP()
 		if err != nil {
 			return
+		} else {
+			conn.Read(p)
+			user.User = string(p)
+			user.Conn = conn
+			users = append(users, user)
+			fmt.Println(users)
+			conn.Write([]byte("Bem vindo " + user.User + " digite /HELP para exibir a lista de comandos\n"))
 		}
-		conn.Write([]byte(welcomeMsg))
-		//for _, c := range conns {
-		//fmt.Println(c, c.RemoteAddr())
-		//}
 		if notIn(conn) {
-			go handleUserConnection(conn, p)
-
+			conns = append(conns, conn)
+			for _, c := range conns {
+				if conn != c {
+					c.Write([]byte(user.User + " entrou no chat\n"))
+				}
+			}
+			go handleUserConnection(conn, p, &users)
 		}
+
 	}
 }
 
-func handleUserConnection(c *net.TCPConn, p []byte) {
-
+func handleUserConnection(c *net.TCPConn, p []byte, users *[]Users) {
 	for {
 		n, err := bufio.NewReader(c).Read(p)
 		if err != nil {
 			fmt.Printf("Some error %v\n", err)
-		}
-		var userMsg UserMsg
-
-		err = json.Unmarshal(p[:n], &userMsg)
-		completAddress := userMsg.IP + ":" + userMsg.Port
-		if userMsg.User != "" {
-			if notIn(c) {
-				conns = append(conns, c)
-				for _, conn := range conns {
-					if conn != c {
-						conn.Write([]byte(userMsg.User + " entrou no chat\n"))
+			conns = removeConnection(conns, c)
+			var username string
+			*users, username = removeUser(users, c)
+			// Se o usarname for vazio, quer dizer que a pessoa usou o comando para sair
+			if username != "" {
+				for _, user := range *users {
+					if user.Conn != c {
+						user.Conn.Write([]byte(username + " desconectou-se\n"))
 					}
 
 				}
 			}
+
+			return
+		}
+		var userMsg UserMsg
+		err = json.Unmarshal(p[:n], &userMsg)
+		completAddress := userMsg.IP + ":" + userMsg.Port
+		if userMsg.User != "" {
 			for _, c := range conns {
 				if userMsg.Body != "" {
-					if c.RemoteAddr().String() != completAddress {
-						c.Write([]byte(userMsg.User + ": " + userMsg.Body))
+					wasInCommandList, index := wasCommandList(userMsg.Body, comands[:])
+					if wasInCommandList {
+						switch index {
+						// /HELP
+						case 0:
+							if c.RemoteAddr().String() == completAddress {
+								c.Write([]byte("Lista de comandos:\n" + comands[1] + "\n" + comands[2] + "\n"))
+							}
+							break
+							// /SAIR
+						case 1:
+							if c.RemoteAddr().String() == completAddress {
+								conns = removeConnection(conns, c)
+								*users, _ = removeUser(users, c)
+								c.Close()
+							} else {
+								c.Write([]byte(userMsg.User + " desconectou-se\n"))
+							}
+							break
+							// /USUARIOS
+						case 2:
+							if c.RemoteAddr().String() == completAddress {
+								fmt.Println(users)
+								for _, user := range *users {
+									c.Write([]byte(user.User + "\n"))
+								}
+							}
+						}
+					} else {
+						if c.RemoteAddr().String() != completAddress {
+							c.Write([]byte(userMsg.User + ": " + userMsg.Body))
+						}
 					}
+
 				}
 
 			}
@@ -87,6 +132,16 @@ func notIn(c *net.TCPConn) bool {
 	return aux == len(conns)
 }
 
+func wasCommandList(msg string, comandList []string) (bool, int) {
+	for index := range comandList {
+		if strings.HasPrefix(msg, comandList[index]) {
+			return true, index
+		}
+	}
+
+	return false, -1
+}
+
 type UserMsg struct {
 	User string
 	Body string
@@ -95,6 +150,28 @@ type UserMsg struct {
 }
 type Users struct {
 	User string
-	IP   string
-	Port string
+	Conn *net.TCPConn
+}
+
+func removeConnection(conns []*net.TCPConn, c *net.TCPConn) []*net.TCPConn {
+	var aux []*net.TCPConn
+	for _, conn := range conns {
+		if c != conn {
+			aux = append(aux, conn)
+		}
+	}
+	return aux
+}
+
+func removeUser(users *[]Users, conn *net.TCPConn) ([]Users, string) {
+	var aux []Users
+	var username string
+	for _, u := range *users {
+		if u.Conn != conn {
+			aux = append(aux, u)
+		} else {
+			username = u.User
+		}
+	}
+	return aux, username
 }
